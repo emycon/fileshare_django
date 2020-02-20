4375ebb1741c1d73387710ca8762fc5dabb753d3
bigmi <emycon@qq.com>
2020-02-20 16:06:05 +0800

    update 1

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,7 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+FILE_CHARSET  = 'gb18030'
+DEFAULT_CHARSET = 'utf-8'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
--- share/models.py
+++ share/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Upload(models.Model):
     DownloadDocount = models.IntegerField(verbose_name=u"Visit Count", default=0)
     code = models.CharField(max_length=8,verbose_name=u"code")
-    Datetime = models.DataTimeField(default=datetime.now.verbose_name=u"Add time")
+    Datetime = models.DateTimeField(default=datetime.now,verbose_name=u"Add time")
     path = models.CharField(max_length=32,verbose_name=u"Download path")
     name = models.CharField(max_length=32,verbose_name=u"Filename",default="")
     Filesize = models.CharField(max_length=10,verbose_name=u"Filesize")
